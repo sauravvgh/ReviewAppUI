da92ea5c1bdf0c556c4dd35ff4e8a322aba43410
saurav ghimire <saurav.ghimire@zakipoint.com>
2022-03-19 09:30:34 +0545

    aded some extra stuffs

---
+++ feedbackapp/src/App.js
@@ -0,0 +1,41 @@
+import Header from "./Components/Header"
+import FeedbackList from './Components/FeedbackList';
+import FeedbackStats from './Components/FeedbackStats';
+import FeedbackForm from './Components/FeedbackForm';
+import AboutPage from './pages/AboutPage';
+import {FeedbackProvider} from './context/FeedbackContext'
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import AboutIconLink from './Components/AboutIconLink';
+
+function App(){
+    return (
+          <FeedbackProvider>
+          <Router>
+              <Header/>  
+              <div className = 'container'>
+          
+              <Routes>
+              <Route exact path='/' element={
+                <>
+                    <FeedbackForm/>
+                    <FeedbackStats/>
+                    <FeedbackList/>
+                </>
+              }>
+              </Route>
+              <Route path ='/about' element = {<AboutPage/>} />
+              </Routes>
+              <AboutIconLink />
+              </div>
+              </Router>
+              
+         </FeedbackProvider>
+             
+          
+           
+            
+       )
+
+}
+
+export default App
